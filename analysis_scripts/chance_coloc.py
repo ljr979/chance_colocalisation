2748300a9e98bc6cf524226f207703b90c526888
ljr979 <ljr979@uowmail.edu.au>
2022-04-26 14:16:54 +1000

    calculating chance colocalisation

--- analysis_scripts/chance_coloc.py
+++ analysis_scripts/chance_coloc.py
@@ -34,22 +34,3 @@ client_coloc_blah = client[client['distance']>-1]
 count_client_coloc=len(client_coloc_blah)
 total_client_molecules = len(client)
 percent_colocal = (count_client_coloc/total_client_molecules)*100
-
-#this way finds the colocalisation for each image (and then 10 replicates essentially)
-percent_colocalisation =[]
-file_number_list=[]
-for filepath in client_coloc:
-
-    file_number=int(filepath.split('/')[-2])
-    file_number_list.append(file_number)
-    data = pd.read_csv(filepath)
-    data.drop([col for col in data.columns.tolist() if ' ' in col], axis=1, inplace=True)
-    client_coloc_count = data[data['distance']>-1]
-    total_coloc_client = len(data)
-    client_coloc_count = len(data[data['distance']>-1])
-    percent_colocal = (client_coloc_count/total_coloc_client)*100
-    percent_colocalisation.append(percent_colocal)
-    
-client=pd.concat(client)
-
-client_coloc_count = client[client['distance']>-1]
